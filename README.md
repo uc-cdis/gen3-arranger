881baa788ce19c9fb9c89dee3166afab9511dd19
Reuben Pasquini <reuben@frickjack.com>
2018-07-11 14:23:36 -0500

    chore(Arranger): initial Arranger integration

--- README.md
+++ README.md
@@ -6,3 +6,23 @@ Gen3 service translates GraphQL requests to Elastic Search queries via the [arra
 
 The @arranger code is licensed under the [AGPL](https://github.com/overture-stack/arranger/blob/master/LICENSE).
 
+## Scripts
+
+Note that the code under `src/` is [typescript](https://www.typescriptlang.org/), 
+so there's a compile step
+to generate the javascript code under `lib/`, `bin/`, and `spec/`.
+The following scripts are registered in `package.json`:
+
+```
+npm run compile
+npm run test
+npm run eslint
+npm start
+```
+
+## Configuration
+
+Override the default configuration with environment variables:
+
+* `GEN3_ES_ENDPOINT` - default is `esproxy-service`
+* `GEN3_PROJECT_ID` - default is `dev` - arranger interacts with the ES index with name `arranger-projects-$Id`
--- src/@gen3/arranger/bin/server.ts
+++ src/@gen3/arranger/bin/server.ts
@@ -1,6 +1,15 @@
 import * as express from 'express';
+import { Server } from 'http';
+import socketIO = require('socket.io');
+import "regenerator-runtime/runtime";
+import Arranger from '@arranger/server';
 import { checkHealth } from '../lib/healthCheck';
+import { singleton as config } from '../lib/config';
+
+
 const app = express();
+const http = new Server(app);
+const io = socketIO(http);
 
 app.get('/_status', async function(req, res) {
   console.log('Processing /_status');
@@ -15,6 +24,20 @@ app.get('/*', function(req, res) {
   res.status(404).json({ "message": "no such path" });
 });
 
-app.listen(3000, function() {
-  console.log('Server listening at http://localhost:3000/');
+const port = 3000;
+
+Arranger({
+  io,
+  projectId: config.projectId,
+  esHost: config.esEndpoint,
+  graphqlOptions: {
+    //context: ({ jwt }) => ({ jwt }),
+    //middleware: [onlyAdminMutations]
+  }
+}).then(router => {
+  app.use(router);
+  http.listen(port, () => {
+    console.log(`⚡️ Listening on port ${port} ⚡️`);
+  });
 });
+
