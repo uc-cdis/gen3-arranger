283e909df538614c716265b9e637f852c671b6b0
Ted Summer <ted.summer2@gmail.com>
2018-11-09 12:57:39 -0600

    debug(healthCheckSpec): add error message for health checks

--- src/@gen3/arranger/spec/healthCheckSpec.ts
+++ src/@gen3/arranger/spec/healthCheckSpec.ts
@@ -3,18 +3,18 @@ import { getHealth, setProjectStarted } from '../lib/healthCheck';
 describe('the healthCheckModule', function() {
   it('should be healthy initially', async function() {
     const status = await getHealth();
-    expect(status.isHealthy).toBe(true);
+    expect(status.isHealthy).toBe(true, `healthStatus: ${JSON.stringify(status)}`);
   });
 
   it('after setProjectStarted true isHealthy true', async function() {
     setProjectStarted(true);
     const status = await getHealth();
-    expect(status.isHealthy).toBe(true);
+    expect(status.isHealthy).toBe(true, `healthStatus: ${JSON.stringify(status)}`);
   });
 
   it('after setProjectStarted false has isHealthy false', async function() {
     setProjectStarted(false);
     const status = await getHealth();
-    expect(status.isHealthy).toBe(false);
+    expect(status.isHealthy).toBe(false, `healthStatus: ${JSON.stringify(status)}`);
   });
 });
