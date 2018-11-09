f0a76052945ad168ecbed4bbf772241451752eec
Ted Summer <ted.summer2@gmail.com>
2018-11-09 10:20:30 -0600

    fix(healthCheckSpec): fix health test and add project started tests

--- src/@gen3/arranger/spec/healthCheckSpec.ts
+++ src/@gen3/arranger/spec/healthCheckSpec.ts
@@ -1,8 +1,20 @@
-import { checkHealth } from '../lib/healthCheck';
+import { getHealth, setProjectStarted } from '../lib/healthCheck';
 
 describe('the healthCheckModule', function() {
   it('should be healthy', async function() {
-    const status = await checkHealth();
+    const status = await getHealth();
     expect(status.isHealthy).toBe(true);
+  });
+
+  it('after setProjectStarted true has projectStarted true', async function() {
+    setProjectStarted(true);
+    const status = await getHealth();
+    expect(status.projectStarted).toBe(true);
+  });
+
+  it('after setProjectStarted false has projectStarted false', async function() {
+    setProjectStarted(false);
+    const status = await getHealth();
+    expect(status.projectStarted).toBe(false);
   });
 });
